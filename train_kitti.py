c9cd9f6f798cfe65e9125329dfd87fb7834e7612
tiago <you@example.com>
2024-02-09 18:48:18 +0000

    update

--- train_kitti.py
+++ train_kitti.py
@@ -35,9 +35,9 @@ input_preprocessing = ' --roi 0 --augmentation 1 --shuffle_points 1 --pcl_norm 0
 resume  = '--resume none'
 
 test_sequrnces = [
-        '--val_set GEORGIA-FR/husky/orchards/10nov23/00/submaps',
-        '--val_set uk/orchards/aut22/extracted',
-        '--val_set uk/strawberry/june23/extracted',
+        #'--val_set GEORGIA-FR/husky/orchards/10nov23/00/submaps',
+        #'--val_set uk/orchards/aut22/extracted',
+        #'--val_set uk/strawberry/june23/extracted',
         '--val_set greenhouse/e3/extracted', 
         '--val_set uk/orchards/sum22/extracted',
         '--val_set uk/orchards/june23/extracted'
