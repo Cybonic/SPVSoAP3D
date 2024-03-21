e1f23e6e5922901818a383de9c679cda9375500b
tiago <tiago.barros.silva@hotmail.com>
2024-03-21 10:49:43 +0000

    update

--- script_eval.py
+++ script_eval.py
@@ -3,17 +3,18 @@ import os
 
 
 # Define the path to the checkpoints
-chkpt_root = 'l/home/tiago/workspace/SPCoV/code/v3/checkpoints/iros24_ablation/cross_validation-nonorm-10m-aug-noroi/triplet/ground_truth_ar0.5m_nr10m_pr2m.pkl/10000/greenhouse-e3-extracted/SPVSoAP3D-SoAP-log-pn-fc-LazyTripletLoss_L2/best_model.pth'
+chkpt_root = '/home/tiago/workspace/SPCoV/code/v3/checkpoints/iros24_ablation/cross_validation-nonorm-10m-aug-noroi/triplet/ground_truth_ar0.5m_nr10m_pr2m.pkl/10000/greenhouse-e3-extracted/SPVSoAP3D-SoAP-log-pn-fc-LazyTripletLoss_L2/best_model.pth'
 
+chkpt_root = '/home/tiago/workspace/SPCoV/checkpoints/iros24_spvsoap_checkpoints'
 save_path = '~/workspace/SPCoV/predictions'
 
 test_sequences = [
-        f'--val_set GTJ23  --resume {chkpt_root}',
-        #f'--val_set OJ22   --resume {chkpt_root}/oj22-spvsoap3d.pth',
-        #f'--val_set OJ23   --resume {chkpt_root}/oj23-spvsoap3d.pth',
-        #f'--val_set ON22   --resume {chkpt_root}/on22-spvsoap3d.pth',
-        #f'--val_set ON23   --resume {chkpt_root}/on23-spvsoap3d.pth',
-        #f'--val_set SJ23   --resume {chkpt_root}/sj23-spvsoap3d.pth', 
+        f'--val_set GTJ23  --resume {chkpt_root}/gtj23-spvsoap3d.pth',
+        f'--val_set OJ22   --resume {chkpt_root}/oj22-spvsoap3d.pth',
+        f'--val_set OJ23   --resume {chkpt_root}/oj23-spvsoap3d.pth',
+        f'--val_set ON22   --resume {chkpt_root}/on22-spvsoap3d.pth',
+        f'--val_set ON23   --resume {chkpt_root}/on23-spvsoap3d.pth',
+        f'--val_set SJ23   --resume {chkpt_root}/sj23-spvsoap3d.pth', 
 ]
 
 experiment = 'iros24/cross_validation'
@@ -23,9 +24,8 @@ for seq in test_sequences:
                 seq,
                 f'--network SPVSoAP3D', # Network
                 '--memory DISK', # [DISK, RAM] 
-                '--device cpu', # Device
+                '--device cuda', # Device
                 '--eval_roi_window 100', # Evaluation ROI window
-                #f'--chkpt_root {chkpt_root}', # Checkpoint root
                 f'--save_predictions {os.path.join(save_path,experiment)}', # Save predictions
                 f'-e {experiment}', # Experiment
                 ' --roi 0 --augmentation 0 --pcl_norm 0' # Preprocessing
